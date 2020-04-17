1f7e07c78b8862f9b756b501270ae5a4464c628b
JavierVelasco_kairosds <javier.velasco@kairosds.com>
2020-04-17 14:20:52 +0200

    prueba de subida correcta a gitLab

--- practica/controllers/controller.js
+++ practica/controllers/controller.js
@@ -1,9 +1,9 @@
-var userchema = require('../models/user')
+// var userchema = require('../models/user')
 
 
-exports.findUser = async function(req, res) {
+// exports.findUser = async function(req, res) {
   
-const datauser = await userchema.find();
-return datauser;
+// const datauser = await userchema.find();
+// return datauser;
 
-};
+// };
